22783509ea27aa31ab5033f73f6baba4349b6aec
Levrone23 <harshpatel23082004@gmail.com>
2025-07-01 05:10:59 +0530

    keypairfile

---
+++ Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINN1mZG22ebpCAUhUjsrvXe64Hq78d8KDXdGFIR4f0Wi Asus@LAPTOP-JKSEGOJL"
+}
